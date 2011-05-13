91f0c1ab86d55102d72092f133266440d1a65635
palach <palach@21c737bc-5872-3cb1-041f-39a7fbbd14bf>
2011-05-13 17:51:56 +0000

    Author: Jan Palach Contact: palach@gmail.com Comment: Structuring project.

---
+++ vtk2webgl/src/tests/test_vtkUnstructuredReader.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+from vtkweb_final.vtkweb.utilities import vtkUnstructuredGrid
+
+
+class VtkUnstructuredGridUnitTest(unittest.TestCase):
+
+    def testRead(self):
+        file = ""
+        assert read(file
+
+
+if __name__ == "__main__":
+    unittest.main()
